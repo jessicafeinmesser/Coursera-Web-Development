d12f787b68cf40fbf27a96391840dac0b136077d
jessicafeinmesser <117769110+jessicafeinmesser@users.noreply.github.com>
2024-11-26 14:58:29 +0200

    Add files via upload

---
+++ module-4-solution/SpeakHello.js
@@ -0,0 +1,15 @@
+
+(function(){
+
+	(function(global){
+		var helloSpeaker = {};
+		var speakWord = "Hello";
+		helloSpeaker.speak = function(name){
+			console.log(speakWord + " " + name);
+		};
+		global.helloSpeaker = helloSpeaker;
+	})(window);
+	
+})();
+
+
